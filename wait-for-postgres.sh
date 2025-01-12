e73739b12af8a53d8ed55665f8b0ae812b4ef1e2
kavon <5903811+kavonjon@users.noreply.github.com>
2025-01-11 18:30:44 -0800

    update deploy config

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -6,7 +6,7 @@ host="$1"
 shift
 cmd="$@"
 
-until PGPASSWORD=$DBPASS psql -h "$host" -U "$DBUSER" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U $POSTGRES_USER -d $POSTGRES_DB -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
