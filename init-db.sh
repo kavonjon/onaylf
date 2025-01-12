61242268b1311dd164bd4a332c6fabadca2ac01b
kavon <5903811+kavonjon@users.noreply.github.com>
2025-01-11 21:39:09 -0800

    update deploy config

--- init-db.sh
+++ init-db.sh
@@ -36,46 +36,9 @@ ls -la /backup/
 echo "Checking if database $DB_NAME exists on port $DB_PORT..."
 DB_EXISTS=$(psql -U postgres -p "$DB_PORT" -tAc "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME'")
 if [[ "$DB_EXISTS" != "1" ]]; then
-  echo "Database $DB_NAME does not exist. Initializing..."
-
-  # Create postgres user if it doesn't exist
-  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER postgres WITH SUPERUSER PASSWORD '$POSTGRES_PASSWORD';
-  EOSQL
-
-  # Create user if it doesn't exist
-  echo "Creating user $DB_USER..."
-  psql -U postgres -p "$DB_PORT" -tAc "DO \$\$
-  BEGIN
-    IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '$DB_USER') THEN
-      CREATE USER $DB_USER WITH PASSWORD '$DB_PASS';
-    END IF;
-  END
-  \$\$;"
-
-  # Create database
-  echo "Creating database $DB_NAME..."
-  psql -U postgres -p "$DB_PORT" -c "CREATE DATABASE $DB_NAME;"
-
-  # Grant privileges
-  echo "Granting privileges to user $DB_USER on database $DB_NAME..."
-  psql -U postgres -p "$DB_PORT" -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
-
-  # Configure user role
-  echo "Configuring role $DB_USER..."
-  psql -U postgres -p "$DB_PORT" -c "ALTER ROLE $DB_USER SET client_encoding TO 'utf8';"
-  psql -U postgres -p "$DB_PORT" -c "ALTER ROLE $DB_USER SET default_transaction_isolation TO 'read committed';"
-  psql -U postgres -p "$DB_PORT" -c "ALTER ROLE $DB_USER SET timezone TO 'UTC';"
-
-  # Grant schema privileges
-  echo "Granting schema privileges to user $DB_USER..."
-  psql -U postgres -p "$DB_PORT" -d "$DB_NAME" -c "GRANT CREATE ON SCHEMA public TO $DB_USER;"
-
+  echo "Database $DB_NAME does not exist. Creating..."
   createdb -U postgres -p "$DB_PORT" "$DB_NAME"
   echo "Database created."
-
-else
-  echo "Database $DB_NAME already exists on port $DB_PORT. Skipping initialization."
 fi
 
 # Debug: Check database emptiness
